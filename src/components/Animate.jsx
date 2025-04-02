2a0679a5426312c310c5be44a155ac78cb44ff70
Prabesh2025 <prabeshneupane231@gmail.com>
2025-04-02 18:34:43 +0545

    update

--- src/components/Animate.jsx
+++ src/components/Animate.jsx
@@ -7,6 +7,7 @@ import hat1Img from "../../src/assets/hat.webp";
 import flower1Img from "../../src/assets/flower.webp";
 import cup1Img from "../../src/assets/cup.webp";
 import music1Img from "../../src/assets/music.webp";
+import Marquee from "react-fast-marquee";
 
 export default function Animate() {
   return (
@@ -14,11 +15,12 @@ export default function Animate() {
          <div className='border-none   mx-auto mt-10 mb-10 shadow-xl bg-gray-50  py-10 text-center'>
             <p className='text-xl font-semibold text-gray-600 hidden lg:block absolute ml-155 z-1'>@PrabeshxStudio</p>
             <p className='text-sm opacity-70 font-medium absolute z-1 ml-130 mt-8 font-mono hidden lg:block'>Join me on Instagram for a peek into my daily news!</p>
-         <button className='border-none rounded-full px-10 py-2 bg-red-400 absolute ml-150 mt-30 z-1 cursor-pointer hover:bg-gray-500 hidden lg:block'>Follow @Prabesh</button>
-        <marquee behavior="animate" direction="left"  classname="animate-bounce">
+         <button className='absolute border-none rounded-full px-10 py-2 bg-red-400  ml-150 mt-30 z-2 cursor-pointer hover:bg-gray-500 hidden lg:block'>Follow @Prabesh</button>
+         <Marquee pauseOnHover={true} speed={30}>
+         
       <div className='flex items-center space-x-2 '>
            
-           <img src={ball1Img} alt="" className='h-50 w-60 object-cover border-none rounded-2xl border-gray-50'/>
+           <img src={ball1Img} alt="" className='h-50 w-60 object-cover border-none rounded-2xl border-gray-50 ml-2'/>
            <img src={car1Img} alt="" className='h-50 w-60 object-cover border-none rounded-2xl border-gray-50 mt-30'/>
            <img src={skateboard1Img} alt="" className='h-50 w-50 object-cover border-none rounded-2xl border-gray-50'/>
            <img src={helmet1Img} alt="" className='h-50 w-40 object-cover border-none rounded-2xl border-gray-50 mt-30'/>
@@ -31,7 +33,8 @@ export default function Animate() {
       </div>
            
         
-        </marquee>
+        
+         </Marquee>
         </div>
         
     </div>
--- src/components/Pages/LifestylePage.jsx
+++ src/components/Pages/LifestylePage.jsx
@@ -1,9 +1,56 @@
 import React from 'react'
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+
+
+// import required modules
+import { EffectFade, Navigation, Pagination } from 'swiper/modules';
+
 export default function LifestylePage() {
   return (
     <div>
-      life
+      
+         
+
+
+         
+        <div className=' '>
+        
+          <Swiper
+            spaceBetween={30}
+            effect={'fade'}
+            navigation={true}
+            pagination={{
+            clickable: true,
+          }}
+        modules={[EffectFade, Navigation, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img className='h-[80vh] w-full object-cover' src="https://swiperjs.com/demos/images/nature-1.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='h-[80vh] w-full object-cover' src="https://swiperjs.com/demos/images/nature-2.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='h-[80vh] w-full object-cover' src="https://swiperjs.com/demos/images/nature-3.jpg" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img className='h-[80vh] w-full object-cover' src="https://swiperjs.com/demos/images/nature-4.jpg" />
+        </SwiperSlide>
+       
+      </Swiper>
+    
+
+        </div>
+
     </div>
   )
 }
