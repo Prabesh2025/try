5cc05dde8078b03898090a73c9e3156320d3af3a
Prabesh2025 <prabeshneupane231@gmail.com>
2025-04-04 23:02:23 +0545

    perviewCarousel

--- src/components/Pages/LifestylePage.jsx
+++ src/components/Pages/LifestylePage.jsx
@@ -1,90 +1,65 @@
-import React from 'react'
+import React from "react";
 
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-fade';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-
-
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 // import required modules
-import { Autoplay, EffectFade, Navigation, Pagination } from 'swiper/modules';
+import { Autoplay, EffectFade, Navigation, Pagination } from "swiper/modules";
+import PerViewCarousel from "../PerViewCarousel";
 
 export default function LifestylePage() {
   return (
-    <div>
+    <div className="space-y-15">
       
-         
-
-
-         
-        <div className=' '>
-        
-          <Swiper
-            spaceBetween={30}
-            effect={'fade'}
-            loop={true}
-
-            autoplay={{
-              delay: 1500,
-              disableOnInteraction: false,
-            }}
-
-            navigation={true}
-            pagination={{
+      <div>
+        <Swiper
+          spaceBetween={30}
+          effect={"fade"}
+          loop={true}
+          autoplay={{
+            delay: 1500,
+            disableOnInteraction: false,
+          }}
+          navigation={true}
+          pagination={{
             clickable: true,
           }}
-        modules={[Autoplay, EffectFade, Navigation, Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-          <img className='h-[80vh] w-full object-cover' src="https://swiperjs.com/demos/images/nature-1.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='h-[80vh] w-full object-cover' src="https://swiperjs.com/demos/images/nature-2.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='h-[80vh] w-full object-cover' src="https://swiperjs.com/demos/images/nature-3.jpg" />
-        </SwiperSlide>
-        <SwiperSlide>
-          <img className='h-[80vh] w-full object-cover' src="https://swiperjs.com/demos/images/nature-4.jpg" />
-        </SwiperSlide>
-       
-      </Swiper>
-    
+          modules={[Autoplay, EffectFade, Navigation, Pagination]}
+          className="mySwiper"
+        >
+          <SwiperSlide>
+            <img
+              className="h-[80vh] w-full object-cover"
+              src="https://swiperjs.com/demos/images/nature-1.jpg"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className="h-[80vh] w-full object-cover"
+              src="https://swiperjs.com/demos/images/nature-2.jpg"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className="h-[80vh] w-full object-cover"
+              src="https://swiperjs.com/demos/images/nature-3.jpg"
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              className="h-[80vh] w-full object-cover"
+              src="https://swiperjs.com/demos/images/nature-4.jpg"
+            />
+          </SwiperSlide>
+        </Swiper>
+      </div>
 
-        </div>
-
-
-          <div className=' h-[50vh]'>
-
-          <>
-      <Swiper
-        slidesPerView={3}
-        spaceBetween={30}
-        pagination={{
-          clickable: true,
-        }}
-        modules={[Pagination]}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
-    </>
-
-          </div>
-
+      <PerViewCarousel />
     </div>
-  )
+  );
 }
---
+++ src/components/PerViewCarousel.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/effect-fade";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+// import required modules
+import { Autoplay, EffectFade, Navigation, Pagination } from "swiper/modules";
+
+
+export default function PerViewCarousel() {
+  return (
+    <div>
+      <Swiper
+       slidesPerView={3}
+        spaceBetween={50}
+        
+        loop={true}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+        }}
+        navigation={true}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Autoplay, Navigation, Pagination]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img
+            className="h-[60vh] w-full object-cover"
+            src="https://swiperjs.com/demos/images/nature-1.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="h-[60vh] w-full object-cover"
+            src="https://swiperjs.com/demos/images/nature-2.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="h-[60vh] w-full object-cover"
+            src="https://swiperjs.com/demos/images/nature-3.jpg"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="h-[60vh] w-full object-cover"
+            src="https://swiperjs.com/demos/images/nature-4.jpg"
+          />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
